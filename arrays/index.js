8c2d9413e84dbdcc46e8177f14f98adc1e4eadf6
usamam46-git <usamam46@gmail.com>
2025-12-10 11:33:52 +0500

    Leetcode 189 solved

--- arrays/index.js
+++ arrays/index.js
@@ -168,6 +168,46 @@ reverseArray([1,2,3,4])
 // twoSum([2,7,11,15], 18)
 
 
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+
+// Output: [5,6,7,1,2,3,4]
+
+// Explanation:
+
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+// Example 2:
+
+// Input: nums = [-1,-100,3,99], k = 2
+
+// Output: [3,99,-1,-100]
+
+// Explanation:
+
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+
+
+
+
+function rotateArray(nums, k) {
+    let reversed = nums.reverse();
+    for (let i = 1; i <= k; i++) {
+        let removed = reversed.shift()
+        reversed.push(removed)
+    }
+    let finalArr = reversed.reverse()
+    return finalArr
+}
+rotateArray([1, 2, 3, 4, 5, 6, 7], 3)
+
 
 
 
