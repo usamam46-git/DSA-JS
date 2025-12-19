21d53d454496f768dd6f5ea1da9afa3f935b7389
usamam46-git <usamam46@gmail.com>
2025-12-19 14:25:33 +0500

    Practicing

--- Binary Tree/index.js
+++ Binary Tree/index.js
@@ -1,146 +1,193 @@
+// // class Node {
+// //     constructor(value) {
+// //         this.value = value
+// //         this.left = null
+// //         this.right = null
+// //     }
+// // }
+
+// // class Tree {
+// //     constructor() {
+// //         this.root = null
+// //     }
+// //     insert() {
+// //         if (this.root === null) {
+
+// //         }
+// //     }
+// // }
+
+
 // class Node {
 //     constructor(value) {
-//         this.value = value
-//         this.left = null
-//         this.right = null
+//         this.value = value;
+//         this.next = null;
 //     }
 // }
 
-// class Tree {
+// class LinkedList {
 //     constructor() {
-//         this.root = null
+//         this.head = null;
 //     }
-//     insert() {
-//         if (this.root === null) {
-          
+
+//     *[Symbol.iterator]() {
+//         let currentNode = this.head;
+//         while (currentNode) {
+//             yield currentNode;
+//             currentNode = currentNode.next;
 //         }
 //     }
 // }
 
+// class Stack {
+//     constructor() {
+//         this.linkedList = new LinkedList();
+//     }
 
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
-}
+//     toString() {
+//         const values = [...this.linkedList].map(node => node.value);
+//         return values.join('\n');
+//     }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
+//     isEmpty() {
+//         return this.linkedList.head === null;
+//     }
 
-    *[Symbol.iterator]() {
-        let currentNode = this.head;
-        while (currentNode) {
-            yield currentNode;
-            currentNode = currentNode.next;
-        }
-    }
-}
+//     push(value) {
+//         const node = new Node(value);
+//         node.next = this.linkedList.head;
+//         this.linkedList.head = node;
+//     }
 
-class Stack {
-    constructor() {
-        this.linkedList = new LinkedList();
-    }
+//     pop() {
+//         if (this.isEmpty()) {
+//             return "There is not any element in the stack";
+//         } else {
+//             const nodeValue = this.linkedList.head.value;
+//             this.linkedList.head = this.linkedList.head.next;
+//             return nodeValue;
+//         }
+//     }
+// }
 
-    toString() {
-        const values = [...this.linkedList].map(node => node.value);
-        return values.join('\n');
-    }
+// class TreeNode {
+//     constructor(value) {
+//         this.value = value;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
 
-    isEmpty() {
-        return this.linkedList.head === null;
-    }
+// class BinaryTree {
+//     constructor() {
+//         this.root = null;
+//     }
 
-    push(value) {
-        const node = new Node(value);
-        node.next = this.linkedList.head;
-        this.linkedList.head = node;
-    }
+//     insert(value) {
+//         let newNode = new TreeNode(value);
 
-    pop() {
-        if (this.isEmpty()) {
-            return "There is not any element in the stack";
-        } else {
-            const nodeValue = this.linkedList.head.value;
-            this.linkedList.head = this.linkedList.head.next;
-            return nodeValue;
-        }
-    }
-}
+//         if (this.root === null) {
+//             this.root = newNode;
+//             return;
+//         }
 
-class TreeNode {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
+//         let queue = [this.root];
+//         while (queue.length > 0) {
+//             let node = queue.shift();
 
-class BinaryTree {
-    constructor() {
-        this.root = null;
-    }
+//             if (node.left === null) {
+//                 node.left = newNode;
+//                 return;
+//             } else {
+//                 queue.push(node.left);
+//             }
 
-    insert(value) {
-        let newNode = new TreeNode(value);
+//             if (node.right === null) {
+//                 node.right = newNode;
+//                 return;
+//             } else {
+//                 queue.push(node.right);
+//             }
+//         }
+//     }
 
-        if (this.root === null) {
-            this.root = newNode;
-            return;
-        }
+//     // preOrder() {
+//     //    if(this.root === null){
+//     //     return
+//     //    }
+//     //    let stack = [this.root]
+//     //    while (stack.length > 0) {
+//     //     let node = stack.pop()
+//     //     console.log(node.value.value)
+//     //     if(node.right !== null){
+//     //         stack.push(node.right)
+//     //     }
+//     //     if(node.left !== null){
+//     //         stack.push(node.left)
+//     //     }
+//     //    }
 
-        let queue = [this.root];
-        while (queue.length > 0) {
-            let node = queue.shift();
-
-            if (node.left === null) {
-                node.left = newNode;
-                return;
-            } else {
-                queue.push(node.left);
-            }
-
-            if (node.right === null) {
-                node.right = newNode;
-                return;
-            } else {
-                queue.push(node.right);
-            }
-        }
-    }
-
-    preOrder() {
-       if(this.root === null){
-        return
-       }
-       let stack = [this.root]
-       while (stack.length > 0) {
-        let node = stack.pop()
-        console.log(node.value.value)
-        if(node.right !== null){
-            stack.push(node.right)
-        }
-        if(node.left !== null){
-            stack.push(node.left)
-        }
-       }
-    }
-}
-
-let node = new Node('N1')
-let node2 = new Node('N2')
-let node3 = new Node('N3')
-let node4 = new Node('N4')
+//     // }
+//     //using recursive approach
+//     preOrder(node = this.root){
+//        if(node !== null){
+//         console.log(node.value)
+//         this.preOrder(node.left)
+//         this.preOrder(node.right)
+//        }
+//     }
 
 
-let tree = new BinaryTree()
-tree.insert(node)
-tree.insert(node2)
-tree.insert(node3)
-tree.insert(node4)
+// }
 
-console.log(tree.preOrder())
+// let node = new Node('N1')
+// let node2 = new Node('N2')
+// let node3 = new Node('N3')
+// let node4 = new Node('N4')
+
+
+// let tree = new BinaryTree()
+// tree.insert(node)
+// tree.insert(node2)
+// tree.insert(node3)
+// tree.insert(node4)
+
+// console.log(tree.preOrder())
+
+
+// console.log("Usama" - 100)
+
+// let arr = [1,2,3,4,5,6]
+// let arr2 = [...arr, 67]
+// console.log(arr2)
+
+
+// let arr = [1,2,'c',5,7,'b','d', 'usama']
+
+// function seperate(array){
+//      let char = []
+//      let numbers = []
+//      for (let i = 0; i < array.length; i++) {
+//         if(typeof array[i] === 'number'){
+//             numbers.push(array[i])
+//         } else if(array[i].length === 1) {
+//             char.push(array[i])
+//         }
+//      }
+//      console.log(numbers)
+//      console.log(char)
+// }
+
+// console.log(seperate(arr))
+
+// let arr = [1, 2, 3, 4, 5, 6]
+
+// let arr2 = arr.forEach((x) =>  x < 5 )
+
+// console.log(arr2)
+
+let arr = [1,2,3,4,5,6]
+let arr2 = JSON.stringify(arr)
+console.log(typeof arr2)
 
 
