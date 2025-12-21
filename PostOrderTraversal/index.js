2ffed347d88f3c624bbb43b9f7aa9a299100c7e0
usamam46-git <usamam46@gmail.com>
2025-12-21 16:38:55 +0500

    Adding recursive approach

--- PostOrderTraversal/index.js
+++ PostOrderTraversal/index.js
@@ -111,12 +111,12 @@ class Node {
         this.next = null;
     }
 }
- 
+
 class LinkedList {
     constructor() {
         this.head = null;
     }
- 
+
     *[Symbol.iterator]() {
         let currentNode = this.head;
         while (currentNode) {
@@ -125,27 +125,27 @@ class LinkedList {
         }
     }
 }
- 
+
 class Stack {
     constructor() {
         this.linkedList = new LinkedList();
     }
- 
+
     toString() {
         const values = [...this.linkedList].map(node => node.value);
         return values.join('\n');
     }
- 
+
     isEmpty() {
         return this.linkedList.head === null;
     }
- 
+
     push(value) {
         const node = new Node(value);
         node.next = this.linkedList.head;
         this.linkedList.head = node;
     }
- 
+
     pop() {
         if (this.isEmpty()) {
             return "There is not any element in the stack";
@@ -156,7 +156,7 @@ class Stack {
         }
     }
 }
- 
+
 class TreeNode {
     constructor(value) {
         this.value = value;
@@ -164,31 +164,31 @@ class TreeNode {
         this.right = null;
     }
 }
- 
+
 class BinaryTree {
     constructor() {
         this.root = null;
     }
- 
+
     insert(value) {
         let newNode = new TreeNode(value);
- 
+
         if (this.root === null) {
             this.root = newNode;
             return;
         }
- 
+
         let queue = [this.root];
         while (queue.length > 0) {
             let node = queue.shift();
- 
+
             if (node.left === null) {
                 node.left = newNode;
                 return;
             } else {
                 queue.push(node.left);
             }
- 
+
             if (node.right === null) {
                 node.right = newNode;
                 return;
@@ -197,14 +197,14 @@ class BinaryTree {
             }
         }
     }
- 
+
     postOrder() {
         if (this.root === null) return;
- 
+
         const stack = new Stack();
         let current = this.root;
         let lastVisitedNode = null;
- 
+
         while (current !== null || !stack.isEmpty()) {
             if (current !== null) {
                 stack.push(current);
@@ -220,6 +220,19 @@ class BinaryTree {
             }
         }
     }
+
+    //Recursive Approach
+
+    postOrderRec(node = this.root) {
+        if (node !== null) {
+            this.postOrderRec(node.left)
+            this.postOrderRec(node.right)
+            console.log(node.value)
+        }
+    }
+
+    //Time Complexity O(N)
+    //Space Complexity O(N)
 }
 
 
