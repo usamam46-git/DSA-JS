7c5555eda138c7b07bafe223bfb13246cfc66ea3
usamam46-git <usamam46@gmail.com>
2025-12-30 13:58:15 +0500

    Practicing array implementation

--- BinaryTreeArrayImplementation/index.js
+++ BinaryTreeArrayImplementation/index.js
@@ -1,22 +1,45 @@
 class BinaryTree {
-    constructor(){
+    constructor() {
         this.arr = []
         this.lastUsedIndex = 0
     }
-    insert(value){
-       this.arr[this.lastUsedIndex + 1] = value
-       this.lastUsedIndex++
+    insert(value) {
+        this.arr[this.lastUsedIndex + 1] = value
+        this.lastUsedIndex++
     }
-    levelOrder(){
+    levelOrder() {
         for (let i = 1; i <= this.lastUsedIndex; i++) {
-            if(this.arr[i] !== undefined){
+            if (this.arr[i] !== undefined) {
                 console.log(arr[i] + "")
             }
-            
+
         }
+    }
+    preOrder(index = 1) {
+        if (index > this.lastUsedIndex || !this.arr[index]) {
+            return
+        }
+        console.log(this.arr[index])
+        this.preOrder(index * 2)
+        this.preOrder(index * 2 + 1)
+    }
+    inOrder(index = 1) {
+        if (index > this.lastUsedIndex || !this.arr[index]) {
+            return
+        }
+        this.inOrder(index * 2)
+        console.log(this.arr[index])
+        this.inOrder(index * 2 + 1)
     }
 }
 
 
 let binaryTree = new BinaryTree()
-console.log(binaryTree)
+binaryTree.insert('N1')
+binaryTree.insert('N2')
+binaryTree.insert('N3')
+binaryTree.insert('N4')
+binaryTree.insert('N5')
+binaryTree.insert('N6')
+binaryTree.insert('N7')
+console.log(binaryTree.inOrder())
