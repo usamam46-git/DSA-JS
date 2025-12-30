0b51dad8d4ad11007965f8abe0488013c253183b
usamam46-git <usamam46@gmail.com>
2025-12-30 13:15:55 +0500

    Practicing traversals

--- BinaryTreeArrayImplementation/index.js
+++ BinaryTreeArrayImplementation/index.js
@@ -3,6 +3,18 @@ class BinaryTree {
         this.arr = []
         this.lastUsedIndex = 0
     }
+    insert(value){
+       this.arr[this.lastUsedIndex + 1] = value
+       this.lastUsedIndex++
+    }
+    levelOrder(){
+        for (let i = 1; i <= this.lastUsedIndex; i++) {
+            if(this.arr[i] !== undefined){
+                console.log(arr[i] + "")
+            }
+            
+        }
+    }
 }
 
 
