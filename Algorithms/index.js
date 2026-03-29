a1d5e7837b7e38c3c830141bb92ace8e03c8dd6e
usamam46-git <usamam46@gmail.com>
2026-03-29 19:32:58 +0500

    Some algorithms

---
+++ Algorithms/index.js
@@ -0,0 +1,55 @@
+// You’re given an array of numbers from 1 to n, but one number is missing. Find it.
+
+function findMissing(arr, n) {
+  const total = (n * (n + 1)) / 2;
+  const sum = arr.reduce((a, b) => a + b, 0);
+  return total - sum;
+}
+
+// First non repeating character.
+
+function firstUniqueChar(str) {
+  const map = {};
+
+  for (let char of str) {
+    map[char] = (map[char] || 0) + 1;
+  }
+
+  for (let char of str) {
+    if (map[char] === 1) return char;
+  }
+
+  return null;
+}
+
+
+// Find indices of two numbers that add up to target.
+
+function twoSum(nums, target) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+
+    if (map[diff] !== undefined) {
+      return [map[diff], i];
+    }
+
+    map[nums[i]] = i;
+  }
+}
+
+
+function flattenArray(arr) {
+  let result = [];
+
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      result = result.concat(flattenArray(item));
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
